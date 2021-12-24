4d1c2d3a8a2c3cea81cc52de4461292373fae409
Nikita Khilioti <n.khilioti@gmail.com>
2021-12-24 11:00:02 +0200

    add jwt token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
 
-constructor(private userService: UsersService) {}
+constructor(
+    private userService: UsersService,
+    private jwtService: JwtService) {}
 
 async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.userService.findOne(username)
@@ -15,6 +18,13 @@ async validateUser(username: string, pass: string): Promise<any> {
     }
     
     return null
+}
+
+async login(user: any) {
+    const payload = {username: user.username, sub: user.userId}
+    return {
+        access_token: this.jwtService.sign(payload)
+    }
 }
 
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,32 +9,44 @@ import {
     Put,
     UseGuards,
     UseInterceptors,
+    Request,
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { TasksService } from './tasks.service';
 import { Task } from './schemas/task.schema';
-import { AuthGuard } from '@nestjs/passport';
-
+import { LocalAuthGuard } from '../auth/local-auth.guard';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('tasks')
 export class TasksController {
     
-    constructor(private readonly taskService: TasksService) {}
+    constructor(
+        private authService: AuthService,
+        private readonly taskService: TasksService
+        ) {}
+
+    @UseGuards(LocalAuthGuard)
+    @Post('auth/login')
+   async login(@Request() req) {
+     return this.authService.login(req.user)
+   }
 
     @Get()
     @UseInterceptors(CacheInterceptor)
     getAllTitles(): Promise<Task[]> {
         return this.taskService.getAllTitles()
     }
-
+   
+    @UseGuards(JwtAuthGuard)
     @Get(':id')
    // @UseInterceptors(CacheInterceptor) // auto cache
     getTask(@Param('id') id: string): Promise<Task> {
         return this.taskService.getTask(id)
     }
 
-    @UseGuards(AuthGuard('local'))
+   // @UseGuards(LocalAuthGuard)
     @Post()
     createTask(@Body() createTask: CreateTaskDto) : Promise<Task> {
         return this.taskService.createTask(createTask)
